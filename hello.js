50fc0f37a3e90fee4b5542284ae584adb4415020
Henrique.Silva <121057131+86HenriqueSilva@users.noreply.github.com>
2023-03-29 22:04:48 -0300

    Update hello.js

--- hello.js
+++ hello.js
@@ -21,3 +21,22 @@ module.exports = {
 }
 
 
+//
+Este é um código para uma função Lambda em Node.js que retorna uma mensagem simples quando acionada. 
+Ele usa o formato de retorno padrão do AWS Lambda que é um objeto com as propriedades statusCode e body.
+
+Aqui está uma explicação das partes importantes do código:
+
+    A primeira linha usa o "modo estrito" do JavaScript para garantir que o código seja executado em um ambiente controlado.
+    A função hello é a função principal que é executada quando a função Lambda é acionada. 
+    Ela recebe um argumento event que contém informações sobre a solicitação que acionou a função.
+    A partir da linha 4, a função retorna um objeto com as propriedades statusCode e body. 
+    O statusCode é definido como 200, o que significa que a solicitação foi bem-sucedida.
+    O body é um objeto JSON que contém uma mensagem e o objeto event recebido como entrada.
+    A partir da linha 11, a função exporta o objeto { handler: hello } como o manipulador da função Lambda.
+    Isso permite que a função seja executada quando a função é acionada.
+//
+
+
+
+
